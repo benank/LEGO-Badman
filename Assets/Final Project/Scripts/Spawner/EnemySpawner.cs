94c9101705cd6b45dd805b3b0b1c3415bbd2097e
Neema Nejad <dnnejad@gmail.com>
2021-05-22 13:44:49 -0700

    added health controller to badman prefab, added enemy spawner script

--- Assets/Final Project/Scripts/Spawner/EnemySpawner.cs
+++ Assets/Final Project/Scripts/Spawner/EnemySpawner.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthController : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private float MaxHealth;
+    [SerializeField] private GameObject enemyPrefab;
+    private Vector3 spawnLocation;
 
     // Start is called before the first frame update
     void Start()
