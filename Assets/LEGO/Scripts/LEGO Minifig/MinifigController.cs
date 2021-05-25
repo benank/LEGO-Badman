8997f3d6766ad1ab46401c8a8145e747d2f91a14
eavivi4 <eavivi@ucdavis.edu>
2021-05-25 13:11:25 -0700

    Test for enemy's health, test for trailer, explode does not work with baton yet merge conflict

--- Assets/LEGO/Scripts/LEGO Minifig/MinifigController.cs
+++ Assets/LEGO/Scripts/LEGO Minifig/MinifigController.cs
@@ -210,6 +210,7 @@ namespace Unity.LEGO.Minifig
         int playSpecialHash = Animator.StringToHash("Play Special");
         int cancelSpecialHash = Animator.StringToHash("Cancel Special");
         int specialIdHash = Animator.StringToHash("Special Id");
+        int badman = Animator.StringToHash("Badman");
 
         Action<bool> onSpecialComplete;
 
@@ -230,10 +231,20 @@ namespace Unity.LEGO.Minifig
 
             // Make sure the Character Controller is grounded if starting on the ground.
             controller.Move(Vector3.down * 0.01f);
+
+            if (gameObject.tag == "Player")
+            {
+                animator.SetBool(badman, true);
+            }
+            else
+            {
+                animator.SetBool(badman, false);
+            }
         }
 
         void Update()
         {
+            // SpecialAnimationFinished();
             if (exploded)
             {
                 return;
@@ -344,6 +355,12 @@ namespace Unity.LEGO.Minifig
                 if (!airborne)
                 {
                     jumpsInAir = maxJumpsInAir;
+                }
+
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    PlaySpecialAnimation(0);
+                    StopSpecialAnimation();
                 }
 
                 // Check if player is jumping.
