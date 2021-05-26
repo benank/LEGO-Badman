60674042330d94300daebfca31232720bc43ed85
eavivi4 <eavivi@ucdavis.edu>
2021-05-26 13:06:51 -0700

    restored minifigcontroller

--- Assets/LEGO/Scripts/LEGO Minifig/MinifigController.cs
+++ Assets/LEGO/Scripts/LEGO Minifig/MinifigController.cs
@@ -210,7 +210,6 @@ namespace Unity.LEGO.Minifig
         int playSpecialHash = Animator.StringToHash("Play Special");
         int cancelSpecialHash = Animator.StringToHash("Cancel Special");
         int specialIdHash = Animator.StringToHash("Special Id");
-        int badman = Animator.StringToHash("Badman");
 
         Action<bool> onSpecialComplete;
 
@@ -231,20 +230,10 @@ namespace Unity.LEGO.Minifig
 
             // Make sure the Character Controller is grounded if starting on the ground.
             controller.Move(Vector3.down * 0.01f);
-
-            if (gameObject.tag == "Player")
-            {
-                animator.SetBool(badman, true);
-            }
-            else
-            {
-                animator.SetBool(badman, false);
-            }
         }
 
         void Update()
         {
-            // SpecialAnimationFinished();
             if (exploded)
             {
                 return;
@@ -355,12 +344,6 @@ namespace Unity.LEGO.Minifig
                 if (!airborne)
                 {
                     jumpsInAir = maxJumpsInAir;
-                }
-
-                if (Input.GetButtonDown("Fire1"))
-                {
-                    PlaySpecialAnimation(0);
-                    StopSpecialAnimation();
                 }
 
                 // Check if player is jumping.
