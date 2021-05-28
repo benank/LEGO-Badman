44a1e78837f10e9b0a43206164cd34ca8048cffc
Neema Nejad <dnnejad@gmail.com>
2021-05-27 20:59:01 -0700

    added sounds to animations and made it to where an animation needs to fully be played before another melee attack can occur

--- Assets/Final Project/Scripts/Combat/CombatController.cs
+++ Assets/Final Project/Scripts/Combat/CombatController.cs
@@ -18,33 +18,71 @@ namespace Combat
     {
         [SerializeField] private GameObject weapon;
         private Animator animator;
-        private MinifigController minifigController;
+        [SerializeField] private AudioClip weaponSound;
+        private AudioSource audioSource;
+        private float meleeCooldown = 0;
+        private float currentAttackDuration = 0;
+        private float swordLungeDuration;
+        private float swordSwipeDuration;
+        private float swordReverseSwipeDuration;
+        
 
         // Start is called before the first frame update
         void Start()
         {
             animator = GetComponent<Animator>();
-            minifigController = GetComponent<MinifigController>();
+            audioSource = GetComponent<AudioSource>();
+            AnimationClip[] clips =  animator.runtimeAnimatorController.animationClips;
+            foreach (AnimationClip clip in clips)
+            {
+                switch (clip.name)
+                {
+                    case "SwordLungeAnimation":
+                        swordLungeDuration = clip.length;
+                        break;
+                    case "OneHandedReverseSwordSwingAnimation":
+                        swordReverseSwipeDuration = clip.length;
+                        break;
+                    case "OneHandedSwordSwingAnimation":
+                        swordSwipeDuration = clip.length;
+                        break;
+                }
+            }
+
         }
 
         // Update is called once per frame
         void Update()
         {
+            // Check if an attack is still being done.
+            if (meleeCooldown <= currentAttackDuration)
+            {
+                meleeCooldown += Time.deltaTime;
+                return;
+            }
+                
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 var values = Enum.GetValues(typeof(MeleeTypes));
                 int random = UnityEngine.Random.Range(0, values.Length);
                 MeleeTypes melee = (MeleeTypes)values.GetValue(random);
+                meleeCooldown = 0;
                 switch (melee)
                 {
                     case MeleeTypes.SwordLunge:
                         animator.Play("SwordLungeAnimation");
+                        audioSource.PlayOneShot(weaponSound);
+                        currentAttackDuration = swordLungeDuration;
                         break;
                     case MeleeTypes.SwordReverseWideSwing:
                         animator.Play("OneHandedReverseSwordSwingAnimation");
+                        audioSource.PlayOneShot(weaponSound);
+                        currentAttackDuration = swordReverseSwipeDuration;
                         break;
                     case MeleeTypes.SwordWideSwing:
                         animator.Play("OneHandedSwordSwingAnimation");
+                        audioSource.PlayOneShot(weaponSound);
+                        currentAttackDuration = swordSwipeDuration;
                         break;
                 }
             }
