b3c4a92268330bd1eca5123ec4a95e5a2842b3e8
Neema Nejad <dnnejad@gmail.com>
2021-06-01 15:49:07 -0700

    added properties to health controller script and assigned default values for each, added health bar to player

--- Assets/Final Project/Scripts/Combat/HealthController.cs
+++ Assets/Final Project/Scripts/Combat/HealthController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
-    [SerializeField] private float MaxHealth;
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int healthRegenRate;
+    [SerializeField] private int healthRegenDelay;
+    [SerializeField] private Slider healthBar;
 
     // Start is called before the first frame update
     void Start()
