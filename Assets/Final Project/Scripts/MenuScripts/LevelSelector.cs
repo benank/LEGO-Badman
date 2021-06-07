8e9581f8a397e7662faaf9f022a26bc2da271219
sbolotin20 <bolotin20@gmail.com>
2021-06-07 12:28:17 -0700

    inventory

--- Assets/Final Project/Scripts/MenuScripts/LevelSelector.cs
+++ Assets/Final Project/Scripts/MenuScripts/LevelSelector.cs
@@ -5,14 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
-   public void LoadMenu()
+    public void MainMenu()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("Main Menu");
     }
+<<<<<<< Updated upstream:Assets/Final Project/Scripts/MenuScripts/LevelSelector.cs
    
    public void LoadLevel1() 
    {
        SceneManager.LoadScene("Level_Tutorial");
    }
+=======
+   public void LoadLevel1_1()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Level_Tutorial");
+    }
+
+    public void LoadLevel2_1()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Level_try");
+    }
+
+    public void LoadLevel2_2()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("VolcanoParkour");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+>>>>>>> Stashed changes:Assets/Final Project/Scripts/LevelSelector.cs
 }
--- Assets/Final Project/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Final Project/Scripts/MenuScripts/MainMenu.cs
@@ -7,8 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
+<<<<<<< Updated upstream:Assets/Final Project/Scripts/MenuScripts/MainMenu.cs
         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         SceneManager.LoadScene("LevelSelection");
+=======
+        SceneManager.LoadScene("LevelSelection");
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+>>>>>>> Stashed changes:Assets/Final Project/Scripts/MainMenu.cs
     }
 
     public void QuitGame()
